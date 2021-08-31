76a54f2b6bd2dbdf2f6caeee243ae99bdde34245
Belorina <ozcan_berrin@hotmail.com>
2021-08-31 10:48:15 +0200

    new script ItemBehavior & id -1 on item prefab & creation tableau

--- Assets/#Project/Scripts/LevelManager.cs
+++ Assets/#Project/Scripts/LevelManager.cs
@@ -12,15 +12,26 @@ public class LevelManager : MonoBehaviour
 
     public GameObject itemPrefab;
 
+    public ItemBehavior[] items;        // tableau 
+
     // Start is called before the first frame update
     void Start()
     {
+        items = new ItemBehavior[row * col];    // creation tableau et grandeur 
+        int index = 0;
+
         for (int x = 0; x < col; x++)           // il faut tenir en compte le gap plustard
         {
             for (int z = 0; z < row; z++)
             {
                 Vector3 position = new Vector3(x * gapCol, 0, z * gapRow);       // etablir la position ( Vector3(x, y, z) )
                 GameObject item = Instantiate(itemPrefab, position, Quaternion.identity);
+
+                items[index] = item.GetComponent<ItemBehavior>();       // avoir ItemBehavior sur le prefab sinon null reference 
+
+                items[index].id = index;            // .id = atteindre l'id de l'object
+
+                index++;
             }
         }
 
