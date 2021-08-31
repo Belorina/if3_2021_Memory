d5a70d8fce253f3ee7c8d4eebfef33bb6aec4f76
Belorina <ozcan_berrin@hotmail.com>
2021-08-31 12:17:37 +0200

    materials contributed

--- Assets/#Project/Scripts/LevelManager.cs
+++ Assets/#Project/Scripts/LevelManager.cs
@@ -12,9 +12,10 @@ public class LevelManager : MonoBehaviour
 
     public GameObject itemPrefab;
 
-    public Material[] Materials;
+    public Material[] materials;
 
     public ItemBehavior[] items;        // tableau 
+    private Dictionary<int, Material> itemMaterial = new Dictionary<int, Material>();     // <key, value>  & instantiate 
 
     // Start is called before the first frame update
     void Start()
@@ -29,13 +30,45 @@ public class LevelManager : MonoBehaviour
                 Vector3 position = new Vector3(x * gapCol, 0, z * gapRow);       // etablir la position ( Vector3(x, y, z) )
                 GameObject item = Instantiate(itemPrefab, position, Quaternion.identity);
 
-                items[index] = item.GetComponent<ItemBehavior>();       // avoir ItemBehavior sur le prefab sinon null reference 
+                items[index] = item.GetComponent<ItemBehavior>();       // avoir ItemBehavior sur le prefab sinon null reference exception
 
                 items[index].id = index;            // .id = atteindre l'id de l'object  -- stockuer les object 
                 items[index].manager = this;        // this = l'instance en cours du script / le LevelManager qui est entrain de jouer 
 
                 index++;
             }
+        }
+
+        GiveMaterials();    // attribuer une couleur a un object
+    }
+
+    private void GiveMaterials()
+    {
+        List<int> possibilities = new List<int>();        // creation list d int, "possibilites"  
+        for (int i = 0; i < row * col; i++)     // i < rom * col = nombre d object
+        {
+            possibilities.Add(i);   // ajouter des chiffre de 0 jusqua nombre d object -1 
+        }
+
+        for (int i = 0; i < materials.Length; i++)      // /!\ on n'utilise pas foreach car declare en momoire une variable, ca va alourdir le pc
+        {
+            if (possibilities.Count < 2) break; // break = sors de la boucle en cours  -- verifie si il y a assez de possibilites 
+
+            int idPos = Random.Range(0, possibilities.Count);    // premier inculive dernier exclusive
+            int id1 = possibilities[idPos];
+            possibilities.RemoveAt(idPos);
+
+            idPos = Random.Range(0, possibilities.Count);       // on ne redeclare pas idPos car deja fait
+            int id2 = possibilities[idPos];
+            possibilities.RemoveAt(idPos);
+
+            itemMaterial.Add(id1, materials[i]);
+            itemMaterial.Add(id2, materials[i]);                // deux identifiant unique, qui ne reapparait plus plustard, et donner le meme material aux deux
+
+            // visuel code 
+            items[id1].GetComponent<Renderer>().material = materials[i];             // renderer = composant de rendue 
+            items[id2].GetComponent<Renderer>().material = materials[i];             // renderer = composant de rendue 
+
         }
 
     }
